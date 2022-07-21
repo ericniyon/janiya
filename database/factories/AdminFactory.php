159c31ccaf0e2cb74884b8026ac67c2d6b8a6eeb
sano20 <audasang2018@gmail.com>
2022-07-21 13:56:42 +0200

    Janiya auth api

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
