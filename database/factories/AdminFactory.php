2b3c38856eb3ed5014263986a5ef69f2ec88f6fe
sano20 <audasang2018@gmail.com>
2022-07-21 14:57:54 +0200

    Janiya auth api

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
@@ -18,14 +17,13 @@ class AdminFactory extends Factory
      */
     public function definition()
     {
-        $factory->define(App\Admin::class, function (Faker $faker) {
-            return [
-                'name' => $this->faker->name(),
-                'email' => 'chrimu25@gmail.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ];
-        });
+        return [
+            'name' => $this->faker->name(),
+            'email' => 'chrimu25@gmail.com',
+            'phone' => $this->faker->unique()->e164PhoneNumber(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
     }
 }
