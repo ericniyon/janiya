e7149d7facc92eb465da99b85ab66e162bd2083a
sano20 <audasang2018@gmail.com>
2022-07-21 14:27:11 +0200

    Janiya auth api

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -18,13 +18,14 @@ class AdminFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => $this->faker->name(),
-            'email' => 'chrimu25@gmail.com',
-            'phone' => $this->faker->unique()->e164PhoneNumber(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        $factory->define(App\Admin::class, function (Faker $faker) {
+            return [
+                'name' => $this->faker->name(),
+                'email' => 'chrimu25@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ];
+        });
     }
 }
